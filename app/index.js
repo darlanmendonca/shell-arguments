0dea89097095b51b0e931e5882943551877866f5
darlanmendonca <darlanmendonca@gmail.com>
2016-01-15 15:37:21 -0200

    better write of index.js

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-module.exports = (function () {
+function parser() {
   var args = process.argv.slice(2);
   args = require('./parser')(args);
 
   return args;
-})();
+}
+
+module.exports = parser();
