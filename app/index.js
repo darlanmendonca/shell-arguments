2715203ebdad582848b52d8604982261d1b3135d
Darlan Mendonça <darlanmendonca@gmail.com>
2016-11-04 18:52:18 -0200

    fix const variable in main file

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 module.exports = parser();
 
 function parser() {
-  const args = process.argv.slice(2);
+  let args = process.argv.slice(2);
   args = require('./parser.es5.js')(args);
 
   return args;
